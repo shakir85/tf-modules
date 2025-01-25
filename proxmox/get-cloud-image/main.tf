1185e4a78b6ef6052f0224e57b6b1c2c21f180a7
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-01-25 13:11:48 -0800

    Upgrade to latest version 0.70.0

--- proxmox/get-cloud-image/main.tf
+++ proxmox/get-cloud-image/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.54.0"
+      version = "0.70.0"
     }
     local = {
       source  = "hashicorp/local"

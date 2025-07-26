d4ffd1b6494699fa6eb54b3e7bc9677dc99d2ede
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-07-25 21:34:58 -0700

    Use Terraform files organization style

---
+++ proxmox/cloud-img-download/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.70.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.1"
+    }
+  }
+}
---
+++ proxmox/vm/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.70.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}

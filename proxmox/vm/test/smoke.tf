07ac1358d4e302642de211c6a47416d3f039cfa8
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-08-02 15:04:31 -0700

    Fix linter version constrains

--- proxmox/vm/test/smoke.tf
+++ proxmox/vm/test/smoke.tf
@@ -10,7 +10,16 @@ terraform {
       version = "0.70.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    tls_private_key = {
+      source  = "hashicorp/tls"
+      version = "3.5.1"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.4.0"
     }
   }
 }

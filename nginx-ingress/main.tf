c3006041b360a6bf1f057d59a46f1a6eaa2db56f
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-08-31 18:31:20 -0700

    Add K8s related modules

---
+++ nginx-ingress/main.tf
@@ -0,0 +1,39 @@
+resource "helm_release" "nginx_ingress" {
+  name             = "nginx-ingress"
+  namespace        = var.kube_namespace
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = var.chart_version
+  create_namespace = var.create_namespace
+
+  values = [
+    yamlencode({
+      controller = {
+        kind = "DaemonSet"
+        ingressClassResource = {
+          name            = var.ingress_controller_name
+          enabled         = true
+          controllerValue = "k8s.io/ingress-nginx"
+        }
+        ingressClass       = var.ingress_controller_name
+        ingressClassByName = true
+        service = {
+          type = "LoadBalancer"
+          port = {
+            http = 80
+          }
+        }
+        fullnameOverride = "nginx-ingress"
+      }
+      labels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      },
+      podLabels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      },
+      commonLabels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      }
+    })
+  ]
+}

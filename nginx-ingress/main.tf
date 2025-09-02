e176b27133c94d64fcc6ece8f5853a84f8787b5b
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-02 11:22:40 -0700

    Restore old values

--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -1,12 +1,50 @@
+terraform {
+  required_version = "~> 1.5.7"
+
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9.0"
+    }
+  }
+}
+
 resource "helm_release" "nginx_ingress" {
-  name             = "nginx-ingress"
+  name             = "ingress-nginx"
   namespace        = var.kube_namespace
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   version          = "4.10.0"
-  create_namespace = var.create_namespace
+  create_namespace = true
 
   values = [
-    file("${path.module}/values.yaml")
+    yamlencode({
+      controller = {
+        kind = "DaemonSet"
+        ingressClassResource = {
+          name            = var.ingress_controller_name
+          enabled         = true
+          controllerValue = "k8s.io/ingress-nginx"
+        }
+        ingressClass       = var.ingress_controller_name
+        ingressClassByName = true
+        service = {
+          type = "LoadBalancer"
+          port = {
+            http = 80
+          }
+        }
+        fullnameOverride = "nginx-controller"
+      }
+      labels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      },
+      podLabels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      },
+      commonLabels = {
+        "app.kubernetes.io/managed-by" = "terraform"
+      }
+    })
   ]
 }

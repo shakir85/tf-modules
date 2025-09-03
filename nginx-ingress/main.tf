34e03fbebc9ab9af1312d0cc9de3a82529773e38
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-02 20:52:17 -0700

    Paramterize namespace creation

--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -4,7 +4,7 @@ resource "helm_release" "nginx_ingress" {
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   version          = "4.10.0"
-  create_namespace = true
+  create_namespace = var.create_namespace
 
   values = [
     yamlencode({

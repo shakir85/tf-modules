4af9fa9f0f0b4ffec17dcf882651f42daa9f4694
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-03 20:58:31 -0700

    Patch module race condition

--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -1,14 +1,21 @@
+resource "kubernetes_namespace" "nginx_ingress" {
+  metadata {
+    name = var.kube_namespace
+  }
+}
+
 resource "helm_release" "nginx_ingress" {
-  name             = "ingress-nginx"
-  namespace        = var.kube_namespace
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  version          = "4.13.1"
-  cleanup_on_fail  = true
-  force_update     = true
-  create_namespace = var.create_namespace
+  name       = "ingress-nginx"
+  namespace  = var.kube_namespace
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.13.1"
+
+  timeout = 600
 
   values = [
     file("${path.module}/values.yaml")
   ]
+
+  depends_on = [kubernetes_namespace.nginx_ingress]
 }

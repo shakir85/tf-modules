45655b921324f7451f380fb01e31073e937a7851
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-03 20:16:58 -0700

    Add cleanup on fail upgrade

--- nginx-ingress/main.tf
+++ nginx-ingress/main.tf
@@ -4,6 +4,7 @@ resource "helm_release" "nginx_ingress" {
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   version          = "4.10.0"
+  cleanup_on_fail  = true
   create_namespace = var.create_namespace
 
   values = [

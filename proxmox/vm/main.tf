58d32a0fbc29671f0c395156c162df93e0b37f92
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-19 17:41:04 -0700

    Improve cloud init config (#96)

--- proxmox/vm/main.tf
+++ proxmox/vm/main.tf
@@ -82,46 +82,18 @@ resource "proxmox_virtual_environment_vm" "vm_resource" {
   serial_device {}
 }
 
-# This section creates a cloud-init configuration file for the VM.
+
 resource "proxmox_virtual_environment_file" "cloud_config" {
   content_type = "snippets"
   datastore_id = var.disk_name
   node_name    = var.proxmox_node_name
 
   source_raw {
-    data = <<EOF
-    #cloud-config
-    hostname: ${var.hostname}
-    write_files:
-      - path: /etc/hosts
-        content: |
-          127.0.0.1 ${var.hostname}
-    users:
-      - default
-      - name: ${var.username}
-        groups:
-          - sudo
-        shell: /bin/bash
-        ssh_authorized_keys:
-          - ${trimspace(data.local_file.ssh_public_key.content)}
-        sudo: ALL=(ALL) NOPASSWD:ALL
-    chpasswd:
-      list: |
-        ${var.username}:${var.temp_user_password}
-    timezone: ${var.timezone}
-    packages:
-      - qemu-guest-agent
-    runcmd:
-      - timedatectl set-timezone ${var.timezone}
-      - systemctl enable qemu-guest-agent --now
-      - echo "done" > /tmp/cloud-config.done
-    EOF
-
+    data = templatefile("${path.module}/cloud-init.yaml", {
+      hostname = var.hostname
+      timezone = var.timezone
+      ssh_key  = trimspace(data.local_file.ssh_public_key.content)
+    })
     file_name = "cloud-init-config-${var.hostname}-${random_string.random_cloud_init_id.result}.yaml"
   }
-}
-
-output "print_temp_password" {
-  description = "Prints the temporary login password defined by `var.temp_user_password`. Check the variable's default value above. You'll be required to change it on your first login."
-  value       = "Your login password is: ${var.temp_user_password}\nYou will be required to change the password on your first login."
 }
